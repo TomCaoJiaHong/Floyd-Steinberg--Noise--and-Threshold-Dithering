bca9b687ed352aef223e90827cbff577fae9ddb1
Jiahong Cao <52592144+TomCaoJiaHong@users.noreply.github.com>
2023-04-22 17:31:31 -0300

    Update noise_dither.m
    Noise dither for 2 and 4 bits

--- noise_dither.m
+++ noise_dither.m
@@ -10,4 +10,15 @@ function dithered_image = noise_dither(image)
     %read image
     im = imread(image);
 
+    %dither image, then set bit depth to 2
+    im_nd = im > rand(size(im));
+    dithered_image = round(((2^2-1)*im_nd)/(2^2-1));
+
+    imwrite(dithered_image, bit2_name);
+
+    %dither image, then set bit depth to 4 
+    im_nd = im > rand(size(im));
+    dithered_image = round(((2^4-1)*im_nd)/(2^4-1));
+
+    imwrite(dithered_image, bit4_name);
 end
